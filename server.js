f13f621913b4556e6d0c3435ad831220130b776f
Lionel Jouin <lionel.jouin@outlook.com>
2018-03-05 18:35:58 +0100

    deploy ci fix

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 const port = process.env.PORT || '3001';
